94aea1588050404404087f0961f4e04d26279658
priyapriyam <priyam18@navgurukul.org>
2019-06-08 00:40:08 +0530

    in this question i used has_key,if i will use this methos it will print boolean value,if there is key so it will print true otherwise false

---
+++ has_key_method.py
@@ -0,0 +1,8 @@
+girl={"Name":"priya","Age":18,"SEX":"female"}
+print girl.has_key("Age")
+priya = {'Name': 'Zara', 'Age': 7}
+print priya.has_key('Age')
+print priya.has_key('Sex')
+d = {'a': 1, 'b': 2}
+print d.has_key('a')
+print d.has_key('d')
